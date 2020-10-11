d421546fd9eaa3e354edc12e2b22437fa04e3c8c
jinho <gogod23@ajou.ac.kr>
2020-10-11 16:49:36 +0900

    add babel & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: "module",
+    },
+    env: {
+        browser: true,
+        es2020: true,
+        node: true,
+    },
+    extends: ["airbnb-base", "plugin:prettier/recommended"],
+
+    rules: {
+        "prettier/prettier": "off",
+        "no-console": "off",
+        "spaced-comment": "off",
+        "no-else-return": "off",
+
+        "no-unused-vars": "off",
+    },
+};
---
+++ src/app.js
@@ -0,0 +1,28 @@
+import path from "path";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.use(helmet({ contentSecurityPolicy: false }));
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use("/static", express.static(path.join(__dirname, "static")));
+
+app.get("/", function (req, res) {
+    res.render("main");
+});
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+export default app;
