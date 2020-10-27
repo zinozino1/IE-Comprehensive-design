75fcf1aaaf1486ea23b5f7610adf50ad24a224ad
jinho <gogod23@ajou.ac.kr>
2020-10-27 21:37:31 +0900

    update search logic

--- src/static/js/searchAjax.js
+++ src/static/js/searchAjax.js
@@ -11,20 +11,87 @@ const resultContainer = document.querySelector("#js-search-result");
 const taskSearchRealTime = function (result) {
     // html element생성하는 함수
     // html 그리는 함수
-
+    let count = 0;
     const column = document.createElement("div");
     column.innerText = "-회사명-               -직무-";
     resultContainer.appendChild(column);
-    result.forEach(function (item) {
+    resultContainer.innerHTML = "";
+
+    for (let i = 0; i < 10; i++) {
         const newDocContainer = document.createElement("div");
         newDocContainer.className = "new-document";
         newDocContainer.style.border = "1px solid black";
-        for (let i = 0; i < 1; i++) {
-            newDocContainer.innerText += `${item[i].company} `;
-            //resultContainer.innerText += `${i}번째 회사 : ${item[i].company} `;
+        for (let j = 0; j < 1; j++) {
+            switch (result[i][j].task) {
+                case `1`:
+                    newDocContainer.innerText += `${result[i][j].company} | 경영 / 사무 / 영업 / 마케팅 / 금융 / 자재 / 기획 `;
+                    break;
+                case `2`:
+                    newDocContainer.innerText += `${result[i][j].company} | IT / 전산 / 네트워크 / 데이터베이스 `;
+                    break;
+                case `3`:
+                    newDocContainer.innerText += `${result[i][j].company} | 생산 / 제조 / 환경 / 플랜트 / 기계 설비 / 공정 / 설계 / 설비 / 품질 `;
+                    break;
+                case `4`:
+                    newDocContainer.innerText += `${result[i][j].company} | 건설 / 건축 / 시공  `;
+                    break;
+                case `5`:
+                    newDocContainer.innerText += `${result[i][j].company} | 유통 / 무역 `;
+                    break;
+                case `6`:
+                    newDocContainer.innerText += `${result[i][j].company} | R&D `;
+                    break;
+                case `7`:
+                    newDocContainer.innerText += `${result[i][j].company} | 전기/전자(설계, 제어) `;
+                    break;
+                case `8`:
+                    newDocContainer.innerText += `${result[i][j].company} | 기타 `;
+                    break;
+                default:
+                    break;
+            }
         }
         resultContainer.appendChild(newDocContainer);
-    });
+    }
+
+    // result.forEach(function (item) {
+    //     const newDocContainer = document.createElement("div");
+    //     newDocContainer.className = "new-document";
+    //     newDocContainer.style.border = "1px solid black";
+    //     for (let i = 0; i < 1; i++) {
+    //         switch (item[i].task) {
+    //             case `1`:
+    //                 newDocContainer.innerText += `${item[i].company} | 경영 / 사무 / 영업 / 마케팅 / 금융 / 자재 / 기획 `;
+    //                 break;
+    //             case `2`:
+    //                 newDocContainer.innerText += `${item[i].company} | IT / 전산 / 네트워크 / 데이터베이스 `;
+    //                 break;
+    //             case `3`:
+    //                 newDocContainer.innerText += `${item[i].company} | 생산 / 제조 / 환경 / 플랜트 / 기계 설비 / 공정 / 설계 / 설비 / 품질 `;
+    //                 break;
+    //             case `4`:
+    //                 newDocContainer.innerText += `${item[i].company} | 건설 / 건축 / 시공  `;
+    //                 break;
+    //             case `5`:
+    //                 newDocContainer.innerText += `${item[i].company} | 유통 / 무역 `;
+    //                 break;
+    //             case `6`:
+    //                 newDocContainer.innerText += `${item[i].company} | R&D `;
+    //                 break;
+    //             case `7`:
+    //                 newDocContainer.innerText += `${item[i].company} | 전기/전자(설계, 제어) `;
+    //                 break;
+    //             case `8`:
+    //                 newDocContainer.innerText += `${item[i].company} | 기타 `;
+    //                 break;
+    //             default:
+    //                 break;
+    //         }
+    //         // newDocContainer.innerText += `${item[i].company}  ${item[i].task}`;
+    //         //resultContainer.innerText += `${i}번째 회사 : ${item[i].company}d `;
+    //     }
+    //     resultContainer.appendChild(newDocContainer);
+    // });
 };
 
 const taskSearchHandler = async function (event) {
