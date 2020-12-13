d321ed77c971a20cf75556003090ba9836c9095e
jinho <gogod23@ajou.ac.kr>
2020-12-13 15:53:08 +0900

    prepare aws deploy

--- src/app.js
+++ src/app.js
@@ -17,8 +17,6 @@ import { localMiddleWare } from "./localMiddleWare";
 import userRouter from "./routers/userRouter";
 import apiRouter from "./routers/apiRouter";
 
-const { PythonShell } = require("python-shell");
-
 dotenv.config();
 const app = express();
 const CookieStore = MongoStore(session);
@@ -53,37 +51,8 @@ app.use("/testPost", (req, res) => {
     console.log(req.body);
     res.end();
 });
-// app.use("/mypage", )
-// app.get("/", function (req, res) {
-//     res.render("main"); // login data
-// });
-// app.get("/login", function (req, res) {
-//     res.render("login");
-// });
-// app.get("/join", function (req, res) {
-//     res.render("join");
-// });
-// app.get("/home", function (req, res) {
-//     res.render("home");
-// });
-// app.get("/mypage", function (req, res) {
-//     res.render("mypage");
-// });
-// app.get("/edit", function (req, res) {
-//     res.render("edit");
-// });
-// app.get("/search", function (req, res) {
-//     res.render("search");
-// });
-// app.get("/select", function (req, res) {
-//     res.render("select");
-// });
-// app.get("/analysis", function (req, res) {
-//     res.render("analysis");
-// });
 
 app.use((req, res, next) => {
     res.render("404");
-    //res.status(404).send("404 NOT FOUND");
 });
 export default app;
