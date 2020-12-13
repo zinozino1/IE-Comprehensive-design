880bf8896cdf1940021a2e68d1d0cd139c9c4c58
jinho <gogod23@ajou.ac.kr>
2020-12-13 16:11:50 +0900

    connect mongodb EC2

--- src/db.js
+++ src/db.js
@@ -12,7 +12,7 @@ mongoose.connect(process.env.MONGO_URL, {
 const db = mongoose.connection;
 
 const handleOpen = () =>
-    console.log("✅ Connected to DB on : //localhost:27017");
+    console.log("✅ Connected to DB on : 52.78.211.1:27017/clover");
 const handleError = () => console.log("❌Error");
 
 db.once("open", handleOpen);
